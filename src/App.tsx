78e39ab01acc2bacd1da669d7f3a6086cbeba2bc
Alvaro Martinez <alvaromfdv@gmail.com>
2024-07-31 20:18:13 +0200

    notesWithTags and onCreateNote

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Container } from "react-bootstrap";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuiV4 } from "uuid";
 
 export type Note = {
   id: string;
@@ -10,7 +12,7 @@ export type Note = {
 
 export type RawNote = {
   id: string;
-};
+} & RawNoteData;
 
 export type RawNoteData = {
   title: string;
@@ -31,6 +33,25 @@ export type Tag = {
 function App() {
   const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
   const [tags, setTags] = useLocalStorage<RawNote[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagsIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuiV4(), tagsIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
   return (
     <Container className="my-4">
       <Routes>
