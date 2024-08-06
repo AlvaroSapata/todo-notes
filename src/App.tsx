8444b1c4aad9f00dd3b592754589ecf887fc237a
Alvaro Martinez <alvaromfdv@gmail.com>
2024-08-06 11:49:25 +0200

    Delete Note Logic

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,10 @@ function App() {
     });
   }
 
+  function onDeleteNote(id: string) {
+    setNotes((prevNotes) => prevNotes.filter((note) => note.id !== id));
+  }
+
   function addTag(tag: Tag) {
     setTags((prev) => [...prev, tag]);
   }
@@ -89,7 +93,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
--- src/Note.tsx
+++ src/Note.tsx
@@ -3,15 +3,11 @@ import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 import ReactMarkdown from "react-markdown";
 
-// type NoteProps = {
-//   onDelete: (id: string) => void;
-// };
+type NoteProps = {
+  onDelete: (id: string) => void;
+};
 
-// export function Note({ onDelete }: NoteProps) {
-//   const note = useNote();
-//   const navigate = useNavigate();
-
-export function Note() {
+export function Note({ onDelete }: NoteProps) {
   const note = useNote();
   const navigate = useNavigate();
 
@@ -35,7 +31,15 @@ export function Note() {
             <Link to={`/${note.id}/edit`}>
               <Button variant="primary">Edit</Button>
             </Link>
-            <Button variant="outline-danger">Delete</Button>
+            <Button
+              onClick={() => {
+                onDelete(note.id);
+                navigate("/");
+              }}
+              variant="outline-danger"
+            >
+              Delete
+            </Button>
             <Link to="/">
               <Button variant="outline-secondary">Back</Button>
             </Link>
@@ -44,42 +48,5 @@ export function Note() {
       </Row>
       <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
-
-    // <>
-    //   <Row className="align-items-center mb-4">
-    //     <Col>
-    //       <h1>{note.title}</h1>
-    //       {note.tags.length > 0 && (
-    //         <Stack gap={1} direction="horizontal" className="flex-wrap">
-    //           {note.tags.map((tag) => (
-    //             <Badge className="text-truncate" key={tag.id}>
-    //               {tag.label}
-    //             </Badge>
-    //           ))}
-    //         </Stack>
-    //       )}
-    //     </Col>
-    //     <Col xs="auto">
-    //       <Stack gap={2} direction="horizontal">
-    //         <Link to={`/${note.id}/edit`}>
-    //           <Button variant="primary">Edit</Button>
-    //         </Link>
-    //         <Button
-    //           onClick={() => {
-    //             onDelete(note.id);
-    //             navigate("/");
-    //           }}
-    //           variant="outline-danger"
-    //         >
-    //           Delete
-    //         </Button>
-    //         <Link to="/">
-    //           <Button variant="outline-secondary">Back</Button>
-    //         </Link>
-    //       </Stack>
-    //     </Col>
-    //   </Row>
-    //   <ReactMarkdown>{note.markdown}</ReactMarkdown>
-    // </>
   );
 }
