29b64750be7cccf7269d730d8d8a7855fccd2be0
Alvaro Martinez <alvaromfdv@gmail.com>
2024-08-06 11:31:21 +0200

    Notes Show page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { useLocalStorage } from "./useLocalStorage";
 import { useMemo } from "react";
 import { v4 as uuidV4 } from "uuid";
 import { NoteList } from "./NoteList";
+import { NoteLayout } from "./NoteLayout";
+import { Note } from "./Note";
 
 export type Note = {
   id: string;
@@ -74,8 +76,8 @@ function App() {
             />
           }
         />
-        <Route path="/:id" element={<h1>new</h1>}>
-          <Route index element={<h1>Show</h1>} />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note/>} />
           <Route path="edit" element={<h1>edit</h1>} />
         </Route>
         <Route path="*" element={<Navigate to="/" />} />
